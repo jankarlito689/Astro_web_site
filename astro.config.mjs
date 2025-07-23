c6502fd8b59a71a5514eb81790d6b3d2e9339b71
cesar jan karlo <leossuarez103@gmail.com>
2025-07-23 13:33:38 -0600

    update and add the dependeci icon

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,11 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
+import astroIcon from 'astro-icon'
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    integrations: [
+        astroIcon({
+            iconDir: './src/icons',
+        }),
+    ],
+});
