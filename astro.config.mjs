68ba5867dee76f02e23f305f548969374119beb8
cesar jan karlo <leossuarez103@gmail.com>
2026-06-06 12:44:15 -0600

    se actulizo y se agrego react

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import astroIcon from 'astro-icon'
+import react from '@astrojs/react';
 // https://astro.build/config
 export default defineConfig({
-    integrations: [
-        astroIcon({
-            iconDir: './src/icons',
-        }),
-    ],
+    integrations: [astroIcon({
+        iconDir: './src/icons',
+    }), react()],
 });
